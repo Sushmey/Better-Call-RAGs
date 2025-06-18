ed2a95f345416174412a12d29d237d2bc04d9986
Sushmey <sushmeywork@gmail.com>
2025-06-18 19:09:08 +0530

    Update File Upload Logic

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.llms import HuggingFacePipeline
+from langchain_community.vectorstores import FAISS
+from langchain_community.llms import HuggingFacePipeline
 from langchain.chains import RetrievalQA
 from transformers import pipeline
 from langchain.llms.base import LLM
@@ -54,24 +54,27 @@ class MixedFileTypeLoader(BaseLoader):
 
     def load(self):
         file_extension = os.path.splitext(self.file_path)[1].lower()
+        print(f"file_extension is ------------ {file_extension} and file path {os.path.splitext(self.file_path)}")
         if file_extension == '.pdf':
             return self._load_pdf()
         elif file_extension in ['.doc', '.docx']:
             return self._load_word()
         elif file_extension == '.txt':
-            return self._load_txt()
+            return TextLoader(
+                file_path = self.file_path,
+                ).load()
         elif file_extension == '.html':
             return self._load_html()
         elif file_extension == '.csv':
             return self._load_csv()
         elif file_extension =='.xml':
-        	return UnstructuredXMLLoader(file_path)
+        	return UnstructuredXMLLoader(self.file_path).load()
         elif file_extension in ['', '/']:
         	return DirectoryLoader(
-				path = r"Documents",
+				path = self.file_path,
 				loader_cls = TextLoader,
 				loader_kwargs = {'autodetect_encoding':True}
-				)
+				).load()
         else:
             raise ValueError(f"Unsupported file type: {file_extension}")
 
@@ -103,18 +106,37 @@ class MixedFileTypeLoader(BaseLoader):
 
 
 @st.cache_resource
-def create_retrieval_qa(folder_path = r'Documents/'): 	
+def initial_retrieval_qa():    
+    # Might have to change the glob regex to look for different kinds of files
+    mixed_loader = MixedFileTypeLoader(file_path = "Documents/")
+
+    try:
+        doc = mixed_loader.load()
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        docs = text_splitter.split_documents(doc)
+        encoder =  HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+        db = FAISS.from_documents(documents=docs, embedding=encoder)
+        retriever = db.as_retriever(search_kwargs = {"k":10})
+
+        gemini_model = GenerativeModel("gemini-2.0-flash")
+        llm = GeminiLLM(model=gemini_model)
+        qa = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
+        return qa
+    except:
+        raise Exception
+
+def create_retrieval_qa(folder_path): 	
 	# Might have to change the glob regex to look for different kinds of files
 	mixed_loader = MixedFileTypeLoader(file_path = folder_path)
 
 	loader = DirectoryLoader(
-			path = r'Documents/',
+			path = r'Documents',
 			loader_cls = TextLoader,
 			loader_kwargs = {'autodetect_encoding':True}
 			)
 
 	try:
-		doc = loader.load()
+		doc = mixed_loader.load();print(type(doc))
 		text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 		docs = text_splitter.split_documents(doc)
 		print(type(docs))
@@ -127,7 +149,7 @@ def create_retrieval_qa(folder_path = r'Documents/'):
 		qa = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
 		return qa
 	except:
-		raise Exception
+		raise
 
 # Show title and description.
 st.title("⚖️ AI Law Assistant")
@@ -135,39 +157,41 @@ st.write(
 	"This is already trained with Cambridge Law Corpus but you can add your own files to add to the RAG! \n"
     "Upload a document below and ask a question about it! \n\n"
     "Note: Loading the dataset for the first time may take some time. \n"
-    "\n"
-    "Sample question: Give a brief summary of the case files provided"
 
+)
+
+
+# Let the user upload a file via `st.file_uploader`.
+uploaded_file = st.file_uploader(
+    "Upload a document", type=["xml","csv","txt"]
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
 
-# Let the user upload a file via `st.file_uploader`.
-uploaded_file = st.file_uploader(
-    "Upload a document", type=("pdf, xml, txt, docx")
-)
+qa = initial_retrieval_qa() # Running it with default dataset
 
-qa = create_retrieval_qa() # Running it with default dataset
 if uploaded_file:
 	try:
-		with tempfile.NamedTemporaryFile(delete=False, suffix=".xml") as tmp:
+		uploaded_file_extension = os.path.splitext(uploaded_file.name)[1].lower()
+		with tempfile.NamedTemporaryFile(delete=False, suffix=uploaded_file_extension) as tmp:
 			tmp.write(uploaded_file.read())
 			tmp_path = tmp.name
 
-		qa = create_retrieval_qa(tmp_path)
+		qa = create_retrieval_qa(tmp_path);print(f"New pipeline created successfully!");st.write("File uploaded successfully")
 		# Remove temporary file after processing
 		os.remove(tmp_path)
 
 	except Exception as e:
-		st.write(f"Error processing email: {str(e)}")
+		print(e)
+		st.write(f"Error processing file: {str(e)}")
 
 
 # Ask the user for a question via `st.text_area`.
 question = st.text_area(
     "Now ask a question about the document!",
-    placeholder="Can you give me a short summary?",
+    placeholder="Give me a brief summary of the case files",
     max_chars=400)
 
 if question:
